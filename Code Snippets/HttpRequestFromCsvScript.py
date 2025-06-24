71d922954425affd7f02565047da735d09018075
Daniel Brito <daniell-henrique@hotmail.com>
2025-06-24 12:24:25 -0300

    Update HttpRequestFromCsvScript.py

--- Code Snippets/HttpRequestFromCsvScript.py
+++ Code Snippets/HttpRequestFromCsvScript.py
@@ -1,23 +1,44 @@
 import requests
 import time
 import csv
+from loguru import logger
+
+def get_access_token(username, password):
+    url = f"<AUTH_URL>"
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "username": username,
+        "password": password
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()
+        return response.json().get('access_token')
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Failed to get access token: {e}")
+        return None
+    
 
 def process_request(id, name, index):
     url = f"<API_URL>"
+    token = get_access_token("username", "password")
     headers = {
-        "Authorization": "Bearer <TOKEN>",
+        "Authorization": f"Bearer {token}",
         "Content-Type": "application/json"
     }
 
-    response = requests.post(url, headers=headers, json={"id": id, "name": name})
-
     with open('logs.txt', 'a') as file:
-        if response.status_code == 200:
-            print(f'{index} Successfull request for id {id}')
-            file.write(f"{index} Successfull request for id {id}\n")
-        else:
-            print(f'{index} Request failed for id {id}: {response.text}')
-            file.write(f"{index} Request failed for id {id}: {response.text}\n")
+        try:
+            response = requests.post(url, headers=headers, json={"id": id, "name": name})
+            response.raise_for_status()
+            
+            logger.info(f"Successfull request for id {id}")
+            file.write(f"Successfull request for id {id}\n")
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Request failed for id {id}: {e}")
+            file.write(f"Request failed for id {id}: {e}\n")
+            return None
 
 def main():
     with open('data.csv', 'r') as file:
